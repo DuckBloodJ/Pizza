43a45e2ed9e86dfaf63f25acd7fed0c34aa4e795
DuckBlood_J <160181383+DuckBloodJ@users.noreply.github.com>
2024-10-12 18:34:22 +0200

    Add files via upload

---
+++ src/src/DatabaseConnection.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/ordering_database";
+    private static final String USER = "jiang";
+    private static final String PASSWORD = "012450";
+
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
